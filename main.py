636a95167a4af9a7ef1fc1b5de47fb1fd58805a8
Per Manholm, jobbet <per.manholm@karlstad.se>
2025-08-18 16:10:36 +0200

    Update window title in PID Simulator to reflect version 1.3

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ class PIDSimulatorApp:
         self.tooltip.place(x=x_root, y=y_root)
     def __init__(self, root):
         self.root = root
-        root.title("PID-simulator v1.3 - Stegsvarsanalys")
+        root.title("PID-simulator v1.3")
         # Öka fönsterbredd för att ge plats åt tooltip
         root.geometry("1100x700")
         root.minsize(1000, 600)
